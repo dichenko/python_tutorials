3502913aac972f63c3bb752fd0c7eb7c6a4f4b97
Dinechko <m@dichenko.ru>
2023-10-13 16:51:07 +0300

    upd OOP

--- OOP/02_Pygame_pong.py
+++ OOP/02_Pygame_pong.py
@@ -11,7 +11,7 @@
 Метод update()
 (Вычисляет новую координату мяча с учетом скорости.  Проверяет, не коснулся ли мяч стенки. Если да - меняет скорость на противоположную)
 Метод draw()
-(Рисует шарик на новых координатах)  - поставить заглушку
+(Рисует шарик на новых координатах)
 
 Класс Paddle:
 Инициализация:
@@ -21,7 +21,7 @@
 Метод update(x: int)
 принимает координату Х от мышки и обновляет свои координаты на новые
 Метод draw()
-(Рисует ракетку на новых координатах)  - поставить заглушку
+(Рисует ракетку на новых координатах)
 Метод collide(ball)
 Принимает обьект мяча, проверяет его координаты, и возвращает True, если мяч коснулся ракетки. Иначе False.
 
@@ -54,6 +54,39 @@ class Ball:
         self.speed = speed
         self.color = color
 
+    def update(self):
+        self.pos[0] += self.speed[0]
+        self.pos[1] += self.speed[1]
+
+        if self.pos[0] - self.radius <= 0 or self.pos[0] + self.radius >= WIDTH:
+            self.speed[0] = -self.speed[0]
+        if self.pos[1] - self.radius <= 0 or self.pos[1] + self.radius >= HEIGHT:
+            self.speed[1] = -self.speed[1]
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, self.pos, self.radius)
+
+
+class Paddle:
+    def __init__(self, x, y, width, height, color):
+        self.pos = [x, y]
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def update(self, x):
+        self.pos[0] = x - self.width // 2
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.pos[0], self.pos[1], self.width, self.height))
+
+    def collide(self, ball):
+        if (
+                self.pos[0] < ball.pos[0] < self.pos[0] + self.width and
+                self.pos[1] - ball.radius < ball.pos[1] < self.pos[1] + self.height
+        ):
+            return True
+        return False
 
 
 ball = Ball(WIDTH // 2, HEIGHT // 2, 20, [2, 2], RED)
@@ -77,104 +110,3 @@ while True: # Основной цикл игры
     paddle.draw(screen) # Прорисовывываем ракетку заново
     pygame.display.flip() # Чтобы все изменения стали видны, нужно флипнуть игровой экран
     pygame.time.delay(15) # Задержка, влияет на плавность анимации. Чем больше, тем хуже анимация
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    """    def update(self):
-            self.pos[0] += self.speed[0]
-            self.pos[1] += self.speed[1]
-
-            if self.pos[0] - self.radius <= 0 or self.pos[0] + self.radius >= WIDTH:
-                self.speed[0] = -self.speed[0]
-            if self.pos[1] - self.radius <= 0 or self.pos[1] + self.radius >= HEIGHT:
-                self.speed[1] = -self.speed[1]
-
-        def draw(self, screen):
-            pygame.draw.circle(screen, self.color, self.pos, self.radius)
-
-
-    class Paddle:
-        def __init__(self, x, y, width, height, color):
-            self.pos = [x, y]
-            self.width = width
-            self.height = height
-            self.color = color
-
-        def update(self, x):
-            self.pos[0] = x - self.width // 2
-
-        def draw(self, screen):
-            pygame.draw.rect(screen, self.color, (self.pos[0], self.pos[1], self.width, self.height))
-
-        def collide(self, ball):
-            if (
-                    self.pos[0] < ball.pos[0] < self.pos[0] + self.width and
-                    self.pos[1] - ball.radius < ball.pos[1] < self.pos[1] + self.height
-            ):
-                return True
-            return False
-    """
