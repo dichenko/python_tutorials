5ab3ac5da6f7f2a683162a9a03e990d5c152eef1
Dinechko <m@dichenko.ru>
2023-11-19 18:20:33 +0300

    fakeUsers

---
+++ requests/01_1_Fake_Users.py
@@ -0,0 +1,18 @@
+# Сайт: https://jsonplaceholder.typicode.com/ создан специально для обучения работы с GET-запросами.
+# Возвращает список из 10 случайных пользователей.
+# TODO
+# Сохранить в текстовый файл список из 10 пользователей, отсортировать их по фамилии
+# Petrov Ivan | Moscow | +79263451881
+
+import requests
+
+url = 'https://jsonplaceholder.typicode.com/users'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print("Список пользователей:", data)
+else:
+    print("Ошибка при выполнении запроса. Код состояния:", response.status_code)
+
